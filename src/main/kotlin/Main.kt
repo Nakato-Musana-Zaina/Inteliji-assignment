98c6dd468d6e442794e1bccc8af2c2571e9e8414
Nakato Musana Zaina <nakatozainamusana@gmail.com>
2024-02-08 21:16:11 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val fullName = "Nakato Musana Zaina"
+    println(fullName)
+    var age = 22
+    println(age)
+    var phoneNumber: Long = +256703588809
+    println(phoneNumber)
+    var weightKg: Byte = 56
+    println(weightKg)
+    val kenyanCitizen: Boolean = false
+    println(kenyanCitizen)
+}
